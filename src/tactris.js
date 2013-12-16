1cb8d7e491afd6607e98bcd610a442dd30079280
birdlab <mail@birdlab.ru>
2013-12-16 17:02:09 +0400

    blocks placing and more

--- src/tactris.js
+++ src/tactris.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     (function(data) {
         var dimensions = data.dimensions,
             viewport = data.viewport,
+            polediv = viewport.children('#pole'),
+            curentdiv = [viewport.find('#next0'), viewport.find('#next1')],
             refs = [
                 [
                     {
@@ -352,30 +354,28 @@ $(document).ready(function() {
             ],
             pole = [],
             userquery = [],
+            score = 0,
             mousedown = false,
             curents = [];
 
-        function newBlock() {
-            var block = $('<div class="tactris-block"></div>').appendTo(viewport);
+
+        function newBlock(container) {
+            var block = $('<div class="tactris-block"></div>').appendTo(container);
+
             block.mousedown(function(e) {
-                mousedown = true;
                 if (block.logicObject.state != 'placed') {
                     block.logicObject.setState('active');
                 }
-                e.stopPropagation();
             });
             block.mouseenter(function(e) {
                 if (mousedown) {
                     if (block.logicObject.state != 'placed') {
                         block.logicObject.setState('active');
                     }
-                    e.stopPropagation();
                 }
-            });
-            block.mouseup(function(e) {
-                mousedown = false;
                 e.stopPropagation();
             });
+
             //change visual state
             block.go = function(state) {
                 block.removeClass('active');
@@ -384,9 +384,12 @@ $(document).ready(function() {
                     block.addClass(state);
                 }
             }
-            var offset = parseInt(block.css('height')) + parseInt(block.css('margin'));
-            console.log(offset);
-            block.css({'top': j * offset + 'px', 'left': i * offset + 'px'});
+            block.setTo = function(logicObject) {
+                this.logicObject = logicObject;
+                var offset = parseInt(block.css('height')) + parseInt(block.css('margin'));
+                block.css({'top': logicObject.y * offset + 'px', 'left': logicObject.x * offset + 'px'});
+               // block.html(logicObject.x + '-' + logicObject.y);
+            }
             return block;
         }
 
@@ -400,7 +403,7 @@ $(document).ready(function() {
                         x: i,
                         y: j,
                         state: 'empty',
-                        div: newBlock(),
+                        div: newBlock(polediv),
                         setState: function(state) {
                             if (state != this.state) {
                                 this.state = state;
@@ -410,17 +413,94 @@ $(document).ready(function() {
                                     checkFigure();
                                 }
                             }
-
+                        },
+                        setPosition: function(x, y, animate) {
+                            this.x = x;
+                            this.y = y;
+                            if (animate) {
+                                this.div.setTo(this);
+                            } else {
+                                this.div.setTo(this);
+                            }
                         }
                     };
-                    block.div.logicObject = block;
+                    block.div.setTo(block);
                     line.push(block);
+                }
+            }
+            var sample = pole[0][0].div,
+                samplesize = parseInt(sample.css('height')) + parseInt(sample.css('margin'));
+            polediv.css({'width': samplesize * dimensions, 'height': samplesize * dimensions});
+        }
+
+        function checkLines() {
+            var outlines = [];
+            for (var j in pole) {
+                var xcounter = 0;
+                var ycounter = 0;
+
+                for (var i in pole[j]) {
+                    if (pole[j][i].state === 'placed') {
+                        xcounter++;
+                    }
+                    if (pole[i][j].state === 'placed') {
+                        ycounter++;
+                    }
+                }
+                if (xcounter == dimensions) {
+                    outlines.push({dir: 'x', line: parseInt(j)});
+                }
+                if (ycounter == dimensions) {
+                    outlines.push({dir: 'y', line: parseInt(j)});
+                }
+            }
+            for (var out in outlines) {
+                var line = outlines[out];
+                if (line.dir === 'x') {
+                    for (var i in pole[line.line]) {
+                        var block = pole[line.line][parseInt(i)];
+                        block.setState('empty');
+                        block.setPosition(parseInt(i), 9, false);
+                    }
+                    pole.push(pole.splice(line.line, 1)[0]);
+                    console.log(pole);
+                    for (var a = line.line; a < dimensions; a++) {
+                        for (var i in pole[a]) {
+                            var b = parseInt(i);
+                            pole[a][b].setPosition(b, a, false);
+                        }
+                    }
+                } else {
+                    for (var i in pole[line.line]) {
+                        var block = pole[i][line.line];
+                        block.setState('empty');
+                    }
+                }
+                score += 10 * outlines.length;
+                viewport.find('#score span').html(score);
+                for (var lines in outlines){
+                    if (outlines[lines].dir==='x'){
+                        outlines[lines].line-=1;
+                    }
                 }
             }
         }
 
         //check user query for similarity
         function checkFigure() {
+            var checkindex = null;
+
+            var instal = function() {
+                for (var a in userquery) {
+                    var block = userquery[a];
+                    block.setState('placed');
+                }
+                userquery = [];
+                setnext(curents[checkindex]);
+                drawnext(curents[checkindex], checkindex);
+                checkLines();
+            }
+
             var ok = function(sx, sy) {
                 for (var a in curents) {
                     var curent = curents[a].figure;
@@ -439,29 +519,25 @@ $(document).ready(function() {
                 }
                 return false;
             }
-            console.log('before check', userquery.length, userquery);
-            if (userquery.length > 2) {
-                console.log('пора проверять');
+            if (userquery.length > 1) {
                 var lowx = dimensions;
                 var lowy = dimensions;
                 for (var a in userquery) {
                     var block = userquery[a];
                     if (block.x < lowx) {
-                        lowx = block.posx;
+                        lowx = block.x;
                     }
                     if (block.y < lowy) {
-                        lowy = block.posy;
+                        lowy = block.y;
                     }
                 }
                 if (ok(lowx, lowy) || ok(lowx - 1, lowy) || ok(lowx, lowy - 1) || ok(lowx - 2, lowy) || ok(lowx, lowy - 2)) {
-                    if (fin && userquery.length == 4) {
-                        //   savestate();
-                        //  score += 4;
-                        //  scorediv.html(score);
-                        //  instal();
-
+                    if (!mousedown && userquery.length == 4) {
+                        //savestate();
+                        score += 4;
+                        viewport.find('#score span').html(score);
+                        instal();
                     }
-                    console.log('угадал');
                 } else {
                     userquery.shift().setState('empty');
                 }
@@ -469,7 +545,7 @@ $(document).ready(function() {
         }
 
         function setnext(nextfigure) {
-            getrandom = function() {
+            var getrandom = function() {
                 var holyrandom = Math.round(Math.random() * (refs.length - 1));
                 if (curents.length == 2) {
                     if (curents[1].refindex == holyrandom || curents[0].refindex == holyrandom) {
@@ -480,18 +556,43 @@ $(document).ready(function() {
                 return holyrandom;
             }
             var hr = getrandom();
-            nextfigure.refindex = hr;
             nextfigure.figure = refs[hr];
+            nextfigure.refindex = hr;
+        }
+
+        function drawnext(nextfigure, container) {
+            var figurecontainer = curentdiv[container].html('');
+            for (var i in nextfigure.figure) {
+                var fig = nextfigure.figure[i];
+                fig.state = 'empty';
+                fig.div = newBlock(figurecontainer);
+                fig.setState = function(state) {
+                    if (state != this.state) {
+                        this.state = state;
+                        this.div.go(state);
+                    }
+                };
+                fig.div.setTo(fig);
+                fig.setState('active');
+            }
         }
 
         function setCurrents() {
             for (var a = 0; a < 2; a++) {
-                var nxt = {refindex: null, figure: []};
+                var nxt = {};
                 curents.push(nxt);
                 setnext(nxt);
+                drawnext(nxt, a);
             }
         }
 
+        $(this).mousedown(function() {
+            mousedown = true;
+        });
+        $(this).mouseup(function() {
+            mousedown = false;
+            checkFigure();
+        });
         initPole();
         setCurrents();
     }(initData));
