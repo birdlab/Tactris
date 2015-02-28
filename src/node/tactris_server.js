a6196351274ae446206ecd5a34e6fd9c14ca15a0
birdlab <mail@birdlab.ru>
2015-02-28 20:59:44 +0300

    sync

--- src/node/tactris_server.js
+++ src/node/tactris_server.js
@@ -1,5 +1,24 @@
 var io = require('socket.io')(40040);
+var http = require('http');
+var options = require('options.json');
+var db = require('db');
 
 io.sockets.on('connection', function(socket) {
-    console.log(socket+ '!! ))');
+    console.log(socket + '!! ))');
+    ;
+    socket.on('login', function(data, callback) {
+        if (data.t) {
+            http.get("http://ulogin.ru/token.php?host=http://birdlab.ru/tactris/html5&token=" + data.t,function(res) {
+                if (res.statusCode == 200) {
+                    res.on('data', function(chunk) {
+                        console.log('BODY: ' + chunk);
+                        var ans = JSON.parse(chunk);
+                        console.log(ans);
+                    });
+                }
+            }).on('error', function(e) {
+                    console.log("Got error: " + e.message);
+                });
+        }
+    });
 });
--- src/www/client.js
+++ src/www/client.js
@@ -1,26 +1,46 @@
 /**
  * Created by Bird on 27.02.15.
  */
-$(document).ready(function() {
-    var tactris = {
-        Client: function() {
-        }
-    };
-    tactris.Client.prototype.socket = io();
+var ulogintoken;
 
-    tactris.Client.prototype.socket.on('connect', function(data) {
-        console.log('connection ok');
+
+var TACTRIS = (function(_t) {
+
+    $(document).ready(function() {
+
+        _t.client = (function() {
+            var client = {};
+            client.uilogintoken = 'asdfasdf';
+            client.socket = io();
+            client.socket.on('connect', function(data) {
+                console.log('connection ok');
+            });
+            client.socket.on('connect_error', function(data) {
+                console.log(data);
+                _t.viewer.showCriticalError(data);
+            });
+            client.login = function(token) {
+                console.log('sending' + token);
+                tactris.client.socket.emit('login', {t: token}, function(data) {
+                    console.log(data);
+                });
+            };
+
+            return client;
+        }());
+
+
     });
-});
+    return _t;
 
+}(TACTRIS || {}));
 
 
 socialcallback = function(token) {
+    tactris.client.login(token);
     $.getJSON("//ulogin.ru/token.php?host=" + encodeURIComponent(location.toString()) + "&token=" + token + "&callback=?",
         function(data) {
-            data = $.parseJSON(data.toString());
-            if (!data.error) {
-                console.log(data);
-            }
+            console.log(data);
+
         });
 }
