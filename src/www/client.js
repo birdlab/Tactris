516a6b2cd510fa7f9ba9a5e594afb9dd125eb3b4
birdlab <mail@birdlab.ru>
2015-03-02 05:12:15 +0300

    temporary single file code

--- src/www/client.js
+++ src/www/client.js
@@ -8,20 +8,44 @@ var TACTRIS = (function(_t) {
 
     $(document).ready(function() {
 
+        _t.viewer = (function() {
+
+            var viewer = {};
+            viewer.showLogin = function() {
+                $('#start .inside').html(' Привет! Для начала трогни одну из кнопочек<div id="uLogin" data-ulogin="display=buttons;fields=first_name,last_name;providers=vkontakte,facebook;redirect_uri=http%3A%2F%2Fbirdlab.ru%2Ftactris%2Fhtml5;callback=login"><div class="loginbutton"><img src="vk.png" data-uloginbutton="vkontakte"/></div><div class="loginbutton"><img src="facebook.png" data-uloginbutton="facebook"/></div></div>');
+            }
+            viewer.showError = function(data) {
+                $('#start .inside').html('<h1>Огорчение!</h1><p>Сервер временно не доступен. Приходите позже. Все наладится )</p><small>' + data + '</small>');
+            }
+            viewer.showGreating = function(data) {
+                $('#start .inside').html('<h1>Привет, ' + data.name + '!</h1>');
+            }
+            viewer.showProgress = function(message) {
+
+                $('#start .inside').html(message);
+            }
+
+            return viewer;
+        }());
+
         _t.client = (function() {
-            var client = {};
-            client.uilogintoken = 'asdfasdf';
-            client.socket = io();
-            client.socket.on('connect', function(data) {
-                console.log('connection ok');
+            var socket = io();
+
+            socket.on('connect', function(data) {
+                _t.viewer.showLogin(data);
             });
-            client.socket.on('connect_error', function(data) {
+            socket.on('connect_error', function(data) {
                 console.log(data);
-                _t.viewer.showCriticalError(data);
+                _t.viewer.showError(data);
             });
+
+
+            var client = {};
+
+
             client.login = function(token) {
-                console.log('sending' + token);
-                tactris.client.socket.emit('login', {t: token}, function(data) {
+                _t.viewer.showProgress('Авторизуем...');
+                socket.emit('login', {t: token}, function(data) {
                     console.log(data);
                 });
             };
@@ -29,18 +53,12 @@ var TACTRIS = (function(_t) {
             return client;
         }());
 
-
     });
+
     return _t;
 
 }(TACTRIS || {}));
 
-
-socialcallback = function(token) {
-    tactris.client.login(token);
-    $.getJSON("//ulogin.ru/token.php?host=" + encodeURIComponent(location.toString()) + "&token=" + token + "&callback=?",
-        function(data) {
-            console.log(data);
-
-        });
+function login(token){
+    TACTRIS.client.login(token);
 }
