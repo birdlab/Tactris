67b29230c5ca973adc3b079303fe30a415775531
birdlab <mail@birdlab.ru>
2015-03-02 05:12:53 +0300

    try work with mongo

--- src/node/db.js
+++ src/node/db.js
@@ -1,4 +1,15 @@
 /**
  * Created by Bird on 28.02.15.
  */
-var options = require('options.json');
+var options = require('./options.json');
+var mongoClient = require('mongodb').MongoClient;
+
+var db = null;
+
+mongoClient.connect('mongodb://127.0.0.1:27017/tactris', function(err, dbinstance) {
+    if (err) {
+        throw err;
+    }
+    console.log('db connected');
+    db = dbinstance;
+})
--- src/node/tactris_server.js
+++ src/node/tactris_server.js
@@ -1,24 +1,30 @@
 var io = require('socket.io')(40040);
 var http = require('http');
-var options = require('options.json');
-var db = require('db');
+var options = require('./options.json');
+var db = require('./db.js');
 
-io.sockets.on('connection', function(socket) {
-    console.log(socket + '!! ))');
-    ;
+
+io.on('connection', function(socket) {
+    console.log(socket.id, 'connected');
     socket.on('login', function(data, callback) {
         if (data.t) {
             http.get("http://ulogin.ru/token.php?host=http://birdlab.ru/tactris/html5&token=" + data.t,function(res) {
                 if (res.statusCode == 200) {
                     res.on('data', function(chunk) {
-                        console.log('BODY: ' + chunk);
-                        var ans = JSON.parse(chunk);
-                        console.log(ans);
+                        var parsedData = JSON.parse(chunk);
+                        db.getSocialUser(parsedData, function(data) {
+                            console.log(data);
+                        })
                     });
+                } else {
+                    console.log(res.statusCode);
+                    callback({error: res.statusCode});
                 }
             }).on('error', function(e) {
-                    console.log("Got error: " + e.message);
+                    callback({error: e.message});
                 });
         }
+    });
+    socket.on('disconnect', function() {
     });
 });
