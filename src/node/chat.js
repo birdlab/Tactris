6189a2d0a91b22c16beb33edbfec45eeca05f283
birdlab <mail@birdlab.ru>
2015-07-17 21:33:51 +0300

    sanitize messages

--- src/node/chat.js
+++ src/node/chat.js
@@ -1,21 +1,25 @@
 /**
  * Created by Bird on 08.07.15.
  */
+
+var sanitizer = require('sanitizer');
+
 function Chat(data) {
-    if (data.game){
-        this.game=data.game;
+    if (data.game) {
+        this.game = data.game;
     }
     this.log = new Array();
 }
 Chat.prototype.broadcastMessage = function(message) {
-    for (var s in this.game.sockets){
-        var socket=this.game.sockets[s];
+    for (var s in this.game.sockets) {
+        var socket = this.game.sockets[s];
         socket.emit('chatmessage', message);
     }
 }
 
 Chat.prototype.postMessage = function(data) {
-    var message = {'m':data.m, name:data.name, 'uid':data.uid};
+    data.m = data.m.replace(/<(?:.|\n)*?>/gm, '');
+    var message = {'m': data.m, name: data.name, 'uid': data.uid};
     this.log.push(message);
     console.log(message);
     this.broadcastMessage(message);
